acd89d560807e199ac468105030a92751b470645
najoua belhaj <najouabelhaj7@gmail.com>
2024-02-12 16:26:15 +0100

    [ ADD QUIZ TEST FUNCT ]

--- src/app/pages/quiz/quiz.component.ts
+++ src/app/pages/quiz/quiz.component.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { MatRadioModule } from '@angular/material/radio';
+import {Job} from "../../interfaces/Job";
+import {Status} from "../../interfaces/Status";
+import {JobService} from "../../services/JobService/job.service";
+import {CompanyService} from "../../services/CompanyService/company.service";
+import {Router} from "@angular/router";
+import {QuizService} from "../../services/QuizService/quiz.service";
+import {Test} from "../../interfaces/test";
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
   styleUrls: ['./quiz.component.css']
 })
-export class QuizComponent {
+export class QuizComponent implements OnInit {
+  tests: Test[] = [];
+  constructor(private quizService: QuizService,private router: Router) {}
+
+  ngOnInit(): void {
+    this.loadTests();
+  }
+  loadTests() {
+    this.quizService.getAllTest().subscribe(
+      (data: Test[]) => {
+        this.tests = data;
+      },
+      error => {
+        console.error('Error loading tests:', error);
+      }
+    );
+  }
 }
